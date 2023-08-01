d697628fbe97eda7ce68f73afcc7d348675c9dfd
yumyeonghan <mgtmh991013@gmail.com>
2023-08-01 22:43:51 +0900

    feat: 1일차

---
+++ src/main/java/com/example/ssec/SsecApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsecApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/ssec/config/WebSecurityConfigure.java
@@ -0,0 +1,23 @@
+package com.example.ssec.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigure {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/me").hasAnyRole("USER", "ADMIN")
+                        .anyRequest().permitAll())
+                .formLogin(auth -> auth.defaultSuccessUrl("/")
+                        .permitAll())
+                .build();
+    }
+}
