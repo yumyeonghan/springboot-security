d598aa8c02ab451c71738435cc3969712a98a465
yumyeonghan <mgtmh991013@gmail.com>
2023-08-09 17:18:21 +0900

    feat: Jdbc 기반 session cluster 구현

--- src/main/java/com/example/ssec/config/WebMvcConfigure.java
+++ src/main/java/com/example/ssec/config/WebMvcConfigure.java
@@ -2,11 +2,13 @@ package com.example.ssec.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableAsync //@Async 사용을 위해 설정
 @Configuration
+@EnableJdbcHttpSession
 public class WebMvcConfigure implements WebMvcConfigurer {
 
     @Override
